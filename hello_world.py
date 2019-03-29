d6c51bc2d0da3f573f514ecaa4995b145fa35af0
msl <train10086@163.com>
2019-03-29 05:01:32 -0700

    adding new file

---
+++ hello_world.py
@@ -0,0 +1,3 @@
+Python 3.6.5 |Anaconda, Inc.| (default, Mar 29 2018, 13:32:41) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello,World!")
